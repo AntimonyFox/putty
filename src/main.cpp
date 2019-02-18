74adbd7a7fee81cb8575f70f30523b56ee524c42
Fox Seligman <aeseligman@gmail.com>
2019-02-18 14:13:53 -0800

    Move main.cpp to src directory

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+
+// #include <iostream>
+// using namespace std;
+
+int main()
+{
+    Game game("game/zork");
+    game.Start();
+
+    // cout << "Hello";
+    // int var;
+    // cin >> var;
+
+    return 0;
+}
