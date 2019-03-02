10e7cd603f21e403e18e896d7ae53ade650ec41b
Zira Fox <aeseligman@gmail.com>
2019-03-01 22:45:51 -0800

    Remove debug lines from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
 #include "Game.h"
 
-// #include <iostream>
-// using namespace std;
-
 int main()
 {
     Game game("game/zork");
     game.Start();
-
-    // cout << "Hello";
-    // int var;
-    // cin >> var;
 
     return 0;
 }
