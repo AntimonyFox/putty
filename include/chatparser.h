8b41d5bcc244d59980965fc612f6226c57be1058
Zira Fox <aeseligman@gmail.com>
2019-03-11 12:34:16 -0700

    Reorganize chatparser.h

--- include/chatparser.h
+++ include/chatparser.h
@@ -1,26 +1,23 @@
 #ifndef CHATPARSER_H
 #define CHATPARSER_H
 
-#include "room.h"
-#include "thing.h"
-#include "game.h"
-
 #ifdef _WIN32
     /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
     #ifndef _WIN32_WINNT
         #define _WIN32_WINNT 0x0501  /* Windows XP. */
     #endif
+    /* winsock2.h must be included first */
     #include <winsock2.h>
-    #include <Ws2tcpip.h>
+
     #include <windows.h>
+    #include <Ws2tcpip.h>
 #else
     /* Use POSIX-style sockets for non-Windows environments. */
-    #include <sys/socket.h>
     #include <arpa/inet.h>
-    #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
-    #include <unistd.h> /* Needed for close() */
-
     #include <cstring>
+    #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+    #include <sys/socket.h>
+    #include <unistd.h> /* Needed for close() */
 
     /* Redefine Windows types for Linux */
     #define SOCKET int
@@ -29,87 +26,87 @@
     #define LPSOCKADDR struct sockaddr *
 #endif
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
-
-#include <thread>
-#include <mutex>
-
 #include <cstdlib>
 #include <iostream>
 #include <map>
 #include <memory>
+#include <mutex>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <thread>
 #include <vector>
+
+#include "room.h"
+#include "thing.h"
+#include "game.h"
 
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "126"
-
 
 using namespace std;
 
 class ChatParser
 {
     public:
-        static string Parse(string parseMe, Player* p);
-        static Game *game;
-        static string ProcessCommand(string data, Player* p);
-        static void SendStringToClient(SOCKET cSock, string message);
+        static void Init(Game* game);
+        static int StartServer();
+
+        /* Client */
+        static string ChatError();
+        static void ClientThread(SOCKET cSock, char* ip);
+        static void LogOff(Player* p);
+        static string NoText();
+        static Player* ReceivePlayerFromLogin(SOCKET cSock);
         static string ReceiveResponse(SOCKET cSock);
         static string ReceiveResponse(SOCKET cSock, string prompt);
-        static Player* ReceivePlayerFromLogin(SOCKET cSock);
-        static void ClientThread(SOCKET cSock, char* ip);
-        static int StartServer();
+        static void SendStringToClient(SOCKET cSock, string message);
+
+        /* Socket */
         static int SocketInit();
         static int SocketGetLastError();
         static int SocketShutdown(SOCKET sock);
         static int SocketClose(SOCKET sock);
         static int SocketQuit();
-        static void LogOff(Player *p);
 
-        static void Init(Game *game);
-        static string ChatError();
-        static string NoText();
+        /* Parsing */
+        static string Parse(string parseMe, Player* p);
+        static string ProcessCommand(string data, Player* p);
 
-        static string Look(Player* p);
-        static string Inventory(Player* p);
-        static void TakeAll(Player* p);
+        /* Player commands */
+        static bool Attack(Player* p, string attackMe, string attackWithMe);
+        static bool Break(string breakMe, string withMe);
+        static string Close(Player* p, Thing* closeMe);
+        static bool Destroy(string destroyMe);
+        static string Diagnose();
+        static string Drink(Player* p, Thing* drinkMe);
+        static string Drop(Player* p, Thing* dropMe);
         static void DropAll(Player* p);
+        static string Eat(Player* p, Thing* eatMe);
+        static string Examine(Player* p, Thing* examineMe);
         static void ExamineAll(Player* p);
-
-        //Steve
-        static bool Throw(Player* p, string throwMe, string hitMe); //done
+        static string In(Player* p, Thing* inMe);
+        static string Inventory(Player* p);
+        static string Jump();
+        static string Look(Player* p);
         static string Move(Player* p, string dir);
+        static string MoveObj(Player* p, Thing* moveMe);
+        static string Open(Player* p, Thing* openMe);
+        static string Pray();
+        static bool Put(Player* p, string putMe, string fillMe);
+        static string Read(Player* p, Thing* readMe);
+        static bool Restore(string state);
+        static bool Save(string state);
+        static string Shout();
+        static bool Swap(string swapMe, string withMe);
+        static string Take(Player* p, Thing* takeMe);
+        static void TakeAll(Player* p);
+        static bool Throw(Player* p, string throwMe, string hitMe);
+        static bool Tie(string tieMe, string toMe);
+        static bool Turn(Player* p, string turnMe);
+        static string Turn(Player* p, Thing* turnMe, bool on);
         static bool Use(Player* p, string useMe);
         static bool Use(Player* p, string useMe, string onMe);
-        static string Drop(Player* p, Thing *dropMe); // done
-        static string Take(Player* p, Thing *takeMe); // done
-        static string Open(Player* p, Thing *openMe); //done
-        static string Read(Player* p, Thing *readMe); //wip
-        static bool Put(Player* p, string putMe, string fillMe);
-
-        //Victor
-        static string Drink(Player* p, Thing *drinkMe); //done
-        static bool Turn(Player* p, string turnMe); //done
-        static string Turn(Player* p, Thing *turnMe, bool on); //done
-        static string MoveObj(Player* p, Thing *moveMe); //done
-        static bool Attack(Player* p, string attackMe, string attackWithMe); //done
-        static string Examine(Player* p, Thing *examineMe); //done
-        static string Eat(Player* p, Thing *eatMe); //done
-        static string Close(Player* p, Thing *closeMe); //done
-        static bool Tie(string tieMe, string toMe); //done
-        static bool Break(string breakMe, string withMe); //done
-        static string In(Player *p, Thing *inMe);
-
-        //Fox
-        static string Jump(); //done
-        static string Pray(); //done
-        static string Diagnose(); //done
-        static string Shout(); //done
-        static bool Destroy(string destroyMe); //wip
-        static bool Swap(string swapMe, string withMe);//wip
-        static bool Save(string state); //wip
-        static bool Restore(string state); //wip
 
         static string lookAlias[3];
         static string inventoryAlias[3];
@@ -149,16 +146,16 @@ class ChatParser
         static string inAlias[3];
 
         static map<string, string> aliasMap;
+        static Game* game;
     private:
+        static string DetermineCommand(vector<string>* arguments);
+        static vector<Thing*> DeterminePossibleThings(Player* p, vector<string>* arguments);
+        static bool InventoryHasThing(Player* p, Thing* thing);
+        static bool RoomHasThing(Player* p, Thing* thing);
+
         static mutex cMutex;
-        static vector<thread> threadList;
-
-        static string DetermineCommand(vector<string> * arguments);
-        static vector<Thing*> DeterminePossibleThings(Player* p, vector<string> * arguments);
-        static bool RoomHasThing(Player* p, Thing *thing);
-        static bool InventoryHasThing(Player* p, Thing *thing);
-
         static string port;
+        static vector<thread> threadList;
 };
 
 #endif // CHATPARSER_H
